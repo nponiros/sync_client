55c13454241e7e084ceabf8e597c967b5e93804c
Nikolas Poniros <nikolas@poniros.de>
2015-12-23 15:26:26 +0100

    add tests

---
+++ test/unit/download_spec.js
@@ -0,0 +1,84 @@
+import download from '../../src/download.js';
+import {CHANGES_DB_STORE_NAME, API_V1_DOWNLOAD, LAST_UPDATE_TS, UPDATE_OPERATION, DELETE_OPERATION} from '../../src/constants.js';
+import * as db from '../../src/indexeddb_connector.js';
+import * as ajax from '../../src/ajax.js';
+import * as DBMock from '../indexeddb_mock.js';
+
+describe('download', () => {
+  function getResolvePromise(toResolveWith) {
+    return new Promise((resolve) => {
+      resolve(toResolveWith);
+    });
+  }
+
+  const collectionName = 'testCollection';
+  const dbName = 'testDB';
+  const collectionNames = [collectionName, CHANGES_DB_STORE_NAME];
+
+  let openDB;
+
+  beforeEach(() => {
+    openDB = new DBMock.IDBDatabase(dbName, collectionNames);
+    spyOn(openDB, 'close');
+    spyOn(db, 'open').and.returnValue(getResolvePromise(openDB));
+    spyOn(db, 'save').and.callThrough();
+    spyOn(db, 'createReadTransaction').and.callThrough();
+    spyOn(db, 'createReadWriteTransaction').and.callThrough();
+    spyOn(db, 'remove').and.callThrough();
+    spyOn(localStorage, 'getItem').and.returnValue(1);
+  });
+
+
+  it('should remove/save data into the store defined by the change object', (done) => {
+    const changeObjects = [{
+      _id: 1,
+      operation: DELETE_OPERATION,
+      collectionName
+    }, {
+      _id: 2,
+      operation: UPDATE_OPERATION,
+      collectionName,
+      changeSet: {
+        _id: 2
+      }
+    }];
+    spyOn(ajax, 'post').and.returnValue(getResolvePromise({changes: changeObjects}));
+    download(dbName, collectionNames).then(() => {
+      expect(db.open).toHaveBeenCalledWith(dbName, collectionNames);
+      expect(db.createReadWriteTransaction.calls.first().args[0]).toEqual(openDB);
+      expect(db.createReadWriteTransaction.calls.first().args[1]).toEqual([collectionName]);
+      expect(typeof db.createReadWriteTransaction.calls.first().args[2]).toBe('function');
+      expect(typeof db.createReadWriteTransaction.calls.first().args[3]).toBe('function');
+      expect(db.remove).toHaveBeenCalledTimes(1);
+      expect(db.save).toHaveBeenCalledTimes(1);
+      expect(db.open).toHaveBeenCalledTimes(1);
+      expect(ajax.post).toHaveBeenCalledWith(API_V1_DOWNLOAD, {lastUpdateTS: 1, collectionNames: [collectionName, CHANGES_DB_STORE_NAME]});
+      expect(openDB.close).toHaveBeenCalledTimes(1);
+      done();
+    }).catch((err) => {
+      done.fail(err);
+    });
+  });
+
+  it('should get the lastUpdateTS from localStorage', (done) => {
+    const changeObjects = [{
+      _id: 1,
+      operation: DELETE_OPERATION,
+      collectionName
+    }, {
+      _id: 2,
+      operation: UPDATE_OPERATION,
+      collectionName,
+      changeSet: {
+        _id: 2
+      }
+    }];
+    spyOn(ajax, 'post').and.returnValue(getResolvePromise({changes: changeObjects}));
+    download(dbName, collectionNames).then(() => {
+      expect(localStorage.getItem).toHaveBeenCalledWith(LAST_UPDATE_TS);
+      done();
+    }).catch((err) => {
+      done.fail(err);
+    });
+  });
+});
--- test/unit/upload_spec.js
+++ test/unit/upload_spec.js
@@ -1,72 +1,84 @@
 import upload from '../../src/upload.js';
+import {CHANGES_DB_STORE_NAME, API_V1_UPLOAD, LAST_UPDATE_TS} from '../../src/constants.js';
 import * as db from '../../src/indexeddb_connector.js';
 import * as ajax from '../../src/ajax.js';
+import * as DBMock from '../indexeddb_mock.js';
 
 describe('upload', () => {
-  const collectionName = 'testCollection';
-  const dbName = 'testDB';
-  const collectionNames = [collectionName, CHANGES_DB_STORE_NAME];
   function getResolvePromise(toResolveWith) {
     return new Promise((resolve) => {
       resolve(toResolveWith);
     });
   }
-  function getRejectPromise(toRejectWith) {
-    return new Promise((resolve, reject) => {
-      reject(toRejectWith);
-    });
-  }
-  function fakeSuccess(openDB, stores, onComplete) {
-    setTimeout(onComplete, 0);
-    return {
-      objectStore(name) {
-        return {name};
-      }
-    };
-  }
-  function fakeError(openDB, stores, onComplete, onError) {
-    setTimeout(onError, 0);
-    return {
-      objectStore(name) {
-        return {name};
-      }
-    };
-  }
+
+  const collectionName = 'testCollection';
+  const dbName = 'testDB';
+  const collectionNames = [collectionName, CHANGES_DB_STORE_NAME];
 
   let openDB;
-  let objectStore;
+
   beforeEach(() => {
-    objectStore = {};
-    openDB = {
-      transaction() {
-        return {
-          objectStore() {
-            return objectStore;
-          }
-        };
-      },
-      close() {
-      }
-    };
+    openDB = new DBMock.IDBDatabase(dbName, collectionNames);
     spyOn(openDB, 'close');
     spyOn(db, 'open').and.returnValue(getResolvePromise(openDB));
+    spyOn(db, 'save').and.callThrough();
     spyOn(db, 'createReadTransaction').and.callThrough();
-    spyOn(ajax, 'post');
+    spyOn(db, 'createReadWriteTransaction').and.callThrough();
+    spyOn(db, 'getAll').and.callThrough();
+    spyOn(db, 'remove');
+    spyOn(localStorage, 'setItem');
   });
 
-  /*it('should call getAll, close the db, send a server request with the data and update the stores with the data from the server', (done) => {
+
+  it('should open the db, call getAll, close the db, send a server request with the data and update the stores with the data from the server', (done) => {
     const data = [{
       _id: 1
+    }, {
+      _id: 2
     }];
-    spyOn(db, 'getAll').and.returnValue(getResolvePromise(data));
+    openDB.setData({
+      [CHANGES_DB_STORE_NAME]: {
+        1: data[0],
+        2: data[1]
+      }
+    });
+    spyOn(ajax, 'post').and.returnValue({lastUpdateTS: 1, changeIds: [1, 2]});
     upload(dbName, collectionNames).then(() => {
       expect(db.open).toHaveBeenCalledWith(dbName, collectionNames);
-      expect(db.createReadTransaction).toHaveBeenCalledWith(openDB, [collectionName]);
-      expect(db.getAll).toHaveBeenCalledWith(objectStore);
-      expect(openDB.close).toHaveBeenCalled();
+      expect(db.createReadTransaction).toHaveBeenCalledWith(openDB, [CHANGES_DB_STORE_NAME]);
+      expect(db.createReadWriteTransaction.calls.first().args[0]).toEqual(openDB);
+      expect(db.createReadWriteTransaction.calls.first().args[1]).toEqual([CHANGES_DB_STORE_NAME]);
+      expect(typeof db.createReadWriteTransaction.calls.first().args[2]).toBe('function');
+      expect(typeof db.createReadWriteTransaction.calls.first().args[3]).toBe('function');
+      expect(db.remove).toHaveBeenCalledTimes(2);
+      expect(db.open).toHaveBeenCalledTimes(2);
+      expect(ajax.post).toHaveBeenCalledWith(API_V1_UPLOAD, {changes: data});
+      expect(openDB.close).toHaveBeenCalledTimes(2);
       done();
     }).catch((err) => {
       done.fail(err);
     });
-  });*/
+  });
+
+  it('should add the lastUpdateTS into localStorage', (done) => {
+    const data = [{
+      _id: 1
+    }, {
+      _id: 2
+    }];
+    openDB.setData({
+      [CHANGES_DB_STORE_NAME]: {
+        1: data[0],
+        2: data[1]
+      }
+    });
+    const lastUpdateTS = 1;
+    spyOn(ajax, 'post').and.returnValue({lastUpdateTS, changeIds: [1, 2]});
+    upload(dbName, collectionNames).then(() => {
+      expect(localStorage.setItem).toHaveBeenCalledWith(LAST_UPDATE_TS, 1);
+      done();
+    }).catch((err) => {
+      done.fail(err);
+    });
+  });
 });
