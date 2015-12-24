db06a5427feb685f11c19793fafeee1ece0dcc5c
Nikolas Poniros <nikolas@poniros.de>
2015-12-24 16:42:43 +0100

    remove the build target from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -18,14 +18,4 @@ gulp.task('eslint', () => {
     .pipe(plugins.eslint.format());
 });
 
-gulp.task('build', () => {
-  return gulp.src('src/**/*.js')
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.babel({
-      presets: ['es2015']
-    }))
-    .pipe(plugins.sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
-});
-
 gulp.task('default', ['eslint']);
