5cd44c059dd85c220bc3cb2272c2856a81b56775
Nikolas Poniros <nikolas@poniros.de>
2015-12-26 13:06:21 +0100

    add start/stop args for the test server

--- test/test_server.js
+++ test/test_server.js
@@ -2,39 +2,54 @@
 
 'use strict';
 
+const fs = require('fs');
+
+const pidFile = '/tmp/sync_server.pid';
+const action = process.argv[2];
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const expressApp = express();
 
-expressApp.use(bodyParser.json());
-expressApp.use(cors());
+if (action === 'start') {
+  fs.writeFileSync(pidFile, process.pid);
 
-expressApp.post('/api/v1/upload', (req, res) => {
-  const changes = req.body.changes;
-  const ids = changes.map((change) => {
-    return change._id;
-  });
-  res.send({
-    changeIds: ids,
-    lastUpdateTS: Date.now()
-  });
-});
+  expressApp.use(bodyParser.json());
+  expressApp.use(cors());
 
-const downloadData = [{
-  _id: 1,
-  collectionName: 'testCollection',
-  operation: 'UPDATE',
-  changeSet: {
+  expressApp.post('/api/v1/upload', (req, res) => {
+    const changes = req.body.changes;
+    const ids = changes.map((change) => {
+      return change._id;
+    });
+    res.send({
+      changeIds: ids,
+      lastUpdateTS: Date.now()
+    });
+  });
+
+  const downloadData = [{
     _id: 1,
-    title: 'title'
-  }
-}];
+    collectionName: 'testCollection',
+    operation: 'UPDATE',
+    changeSet: {
+      _id: 1,
+      title: 'title'
+    }
+  }];
 
-expressApp.post('/api/v1/download', (req, res) => {
-  res.send({
-    changes: downloadData
+  expressApp.post('/api/v1/download', (req, res) => {
+    res.send({
+      changes: downloadData
+    });
   });
-});
 
-expressApp.listen(3000);
+  expressApp.listen(3000);
+} else if (action === 'stop') {
+  const pid = fs.readFileSync(pidFile);
+  process.kill(Number(pid));
+  fs.unlinkSync(pidFile);
+} else {
+  throw Error('Action not supported!');
+}
