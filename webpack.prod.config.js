57aa1f50b80996e7e718cb31e8851830b82b045e
Nikolas Poniros <nikolas@poniros.de>
2016-12-15 15:57:55 +0100

    update dependencies

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,6 @@
+const webpack = require('webpack');
 const path = require('path');
+
 module.exports = {
   entry: './src/sync_client.js',
   devtool: 'source-map',
@@ -6,14 +8,49 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'syncClient.min.js',
     libraryTarget: 'commonjs2',
-    library: true
+    library: 'syncClient.min.js',
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: path.join(__dirname, 'src'),
-        loader: 'babel?presets[]=es2015'
-      }
-    ]
-  }
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          plugins: ['transform-runtime'],
+          presets: [
+            ['es2015', { modules: false }],
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') },
+    }),
+  ],
+  resolve: {
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+    ],
+  },
 };
