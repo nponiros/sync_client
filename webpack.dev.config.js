3939575f7ccd699d8f6193c36e1bcaf5827be093
Nikolas Poniros <nikolas@poniros.de>
2016-12-17 15:32:50 +0100

    remove uglifyjs

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -26,23 +26,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true,
-      },
-      output: {
-        comments: false,
-      },
-    }),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify('development') },
     }),
