b5ec95018cf533092171da469fc9285996688fb9
Nikolas Poniros <nikolas@poniros.de>
2017-01-15 14:24:33 +0100

    Add source map to uglifyjs

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -42,6 +42,7 @@ module.exports = {
       output: {
         comments: false,
       },
+      sourceMap: true,
     }),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify('production') },
